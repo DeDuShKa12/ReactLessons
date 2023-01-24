5f1243f03e435828a24172c805956ff034d0c639
DeDuShKa12 <pasharudiuk97@gmail.com>
2023-01-24 19:28:08 +0300

    HW

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {CarForm, Cars} from "./component";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [car, setCar] = useState([]);
+
+    return (
+        <div className="App">
+            <CarForm/>
+            <hr/>
+            <Cars setCars={setCar}/>
+        </div>
+    );
 }
 
 export default App;

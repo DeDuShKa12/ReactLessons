4e6e0070be160be09a6a6811c46d050da63b01b4
DeDuShKa12 <pasharudiuk97@gmail.com>
2023-01-24 19:30:12 +0300

    Main

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import {CarForm, Cars} from "./component";
-import {useState} from "react";
 
 function App() {
-    const [car, setCar] = useState([]);
+
 
     return (
         <div className="App">
-            <CarForm/>
-            <hr/>
-            <Cars setCars={setCar}/>
+
         </div>
     );
 }
